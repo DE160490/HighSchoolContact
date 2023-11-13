06d8b32316c62ad1f3170627bc40eee65e8c2483
Nguyễn Công Thành <nguyencongthanh15082001@gmail.com>
2023-11-13 10:44:59 +0700

    Update Frontend Second#

---
+++ FBT/wwwroot/js/login.js
@@ -0,0 +1,22 @@
+﻿const loginButton = document.querySelector('.button_login');
+
+loginButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const accountIdInput = document.getElementById('AccountId');
+    const passwordInput = document.getElementById('Password');
+
+    if (accountIdInput.value === '') {
+        alert('Please enter your account ID.');
+        accountIdInput.focus();
+        return;
+    }
+
+    if (passwordInput.value === '') {
+        alert('Please enter your password.');
+        passwordInput.focus();
+        return;
+    }
+
+    // Submit the form if both inputs are not empty
+    loginButton.form.submit();
+});
