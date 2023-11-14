9060b38e6b5f0b4fce81155e25f71bb54481f15d
Nguyễn Công Thành <nguyencongthanh15082001@gmail.com>
2023-11-14 15:27:56 +0700

    Update Login

--- FBT/Controllers/LoginController.cs
+++ FBT/Controllers/LoginController.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FBT.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FBT.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(Account account) {
+            return View(account);
+        }
 
     }
 }
