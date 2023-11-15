6e7261333715470baf691cd4241a048f44e00c22
Nguyễn Công Thành <nguyencongthanh15082001@gmail.com>
2023-11-15 09:08:10 +0700

    Update role

--- FBT/Controllers/LoginController.cs
+++ FBT/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using FBT.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FBT.Controllers
 {
@@ -12,7 +13,37 @@ namespace FBT.Controllers
 
         [HttpPost]
         public IActionResult Login(Account account) {
-            return View(account);
+            using (var dbContext = new FbtContext())
+            {
+                Console.WriteLine(account + " " + account.AccountId + " " + account.Password);
+                Account? checkAccount = dbContext.Accounts.FirstOrDefault(s => s.AccountId == account.AccountId);
+                if (checkAccount != null)
+                {
+                    HttpContext.Session.SetString("username", account.AccountId);
+                    if(checkAccount.Role == 0)
+                    {
+                        return RedirectToAction("Index", "Student", new { studentId = account.AccountId});
+                    }else if(checkAccount.Role == 1)
+                    {
+                        return RedirectToAction("Index", "Teacher", new { teacherId = account.AccountId });
+                    }else if(checkAccount.Role == 2)
+                    {
+                        return RedirectToAction("Index", "Parent", new { parentId = account.AccountId });
+                    }else if(checkAccount.Role == 3)
+                    {
+                        return RedirectToAction("Index", "Admin", new { adminId = account.AccountId });
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                    return View(account);
+                }
+                else
+                {
+                    return View(account);
+                }
+            }
         }
 
     }
