0d355af6d1f48b4993206acc1c8a31035727f3fc
trumdeptrai123@gmail.com <trumdeptrai123@gmail.com>
2023-11-17 16:34:40 +0700

    crud Account, them Update, Delete, Detail by Le Trung Nghia

--- FBT/Controllers/AdminController.cs
+++ FBT/Controllers/AdminController.cs
@@ -12,11 +12,15 @@ public class AdminController : Controller
     {
         using (var dbContext = new FbtContext())
         {
-            List<Account> objAccountList = dbContext.Accounts.Include(a => a.AccountNavigation).ToList();
+            List<Account> objAccountList = dbContext.Accounts
+                .Include(a => a.AccountNavigation)
+                .Where(a => a.Role != 3)
+                .ToList();
             return View(objAccountList);
         }
     }
 
+    //---------------------------------------CREATE
 
     public IActionResult Create()
     {
@@ -26,14 +30,41 @@ public class AdminController : Controller
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Create([Bind("Id,Fullname,Gender,Dob,PlaceOfBirth,PlaceOfResidence,Phone,Email,Ethnic,Religion")] PersonInformation personInformation, string Password)
+    public IActionResult Create([Bind("Id,Fullname,Gender,Dob,PlaceOfBirth,PlaceOfResidence,Phone,Email,Ethnic,Religion")] PersonInformation personInformation, string Password, string Role)
     {
         using (var dbContext = new FbtContext())
         {
             ModelState.Remove("Account");
+            ModelState.Remove("Id");
+
+            var lastId = dbContext.PersonInformations
+                .Where(p => p.Id.StartsWith(Role))
+                .OrderByDescending(p => p.Id)
+                .Select(p => p.Id)
+                .FirstOrDefault();
+
+            if (lastId == null)
+            {
+                personInformation.Id = Role + "0000001";
+            }
+            else
+            {
+                var numberPart = int.Parse(lastId.Substring(3));
+                var nextNumberPart = (numberPart + 1).ToString("D7");
+                personInformation.Id = Role + nextNumberPart;
+            }
+
+            if (dbContext.PersonInformations.Any(p => p.Id == personInformation.Id))
+            {
+                ModelState.AddModelError("Id", "A person with this Id already exists.");
+            }
+            //if (dbContext.PersonInformations.Any(p => p.Id == personInformation.Id))
+            //{
+            //    ModelState.AddModelError("Id", "A person with this Id already exists.");
+            //}
             if (ModelState.IsValid)
             {
-                // Add the PersonInformation to the context and save changes
+
                 dbContext.PersonInformations.Add(personInformation);
                 dbContext.SaveChanges();
 
@@ -45,12 +76,11 @@ public class AdminController : Controller
 
                 // Create the Account
                 var account = personInformation.Account;
-                account.AccountId = personInformation.Id;  // Assign PersonInformation.Id to Account.AccountId
+                account.AccountId = personInformation.Id;
                 //account.Password = "password123";
                 account.Password = Password;
-                //account.Password = personInformation.Account.Password;
 
-                // Determine the role based on the ID prefix
+
                 string idPrefix = personInformation.Id.Substring(0, 3);
                 if (idPrefix == "HSS")
                 {
@@ -66,18 +96,164 @@ public class AdminController : Controller
                 }
                 else
                 {
-                    account.Role = -1; // Default value
+                    account.Role = -1;
                 }
 
-                // Add the Account to the context and save changes
                 dbContext.Accounts.Add(account);
                 dbContext.SaveChanges();
 
                 return RedirectToAction("Index", "Admin");
             }
 
-            // If the model state is not valid, return the view with the current PersonInformation
             return View(personInformation);
+        }
+    }
+
+    //--------------------------------------------------EDIT
+    // GET: Admin/Edit/5
+    public IActionResult Edit(string id)
+    {
+        using (var dbContext = new FbtContext())
+        {
+            var personInformation = dbContext.PersonInformations
+                .Include(p => p.Account)
+                .SingleOrDefault(p => p.Id == id);
+
+            if (personInformation == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Password"] = personInformation.Account?.Password;
+
+            return View("Edit", personInformation);
+        }
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(string id, [Bind("Id,Fullname,Gender,Dob,PlaceOfBirth,PlaceOfResidence,Phone,Email,Ethnic,Religion")] PersonInformation personInformation, string Password)
+    {
+        using (var dbContext = new FbtContext())
+        {
+            ModelState.Remove("Account");
+
+            if (id != personInformation.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var existingPersonInformation = dbContext.PersonInformations
+                        .Include(p => p.Account)
+                        .FirstOrDefault(p => p.Id == id);
+
+                    if (existingPersonInformation == null)
+                    {
+                        return NotFound();
+                    }
+
+
+                    personInformation.Id = id;
+                    existingPersonInformation.Id = personInformation.Id;
+                    existingPersonInformation.Fullname = personInformation.Fullname;
+                    existingPersonInformation.Gender = personInformation.Gender;
+                    existingPersonInformation.Dob = personInformation.Dob;
+                    existingPersonInformation.PlaceOfBirth = personInformation.PlaceOfBirth;
+                    existingPersonInformation.PlaceOfResidence = personInformation.PlaceOfResidence;
+                    existingPersonInformation.Phone = personInformation.Phone;
+                    existingPersonInformation.Email = personInformation.Email;
+                    existingPersonInformation.Ethnic = personInformation.Ethnic;
+                    existingPersonInformation.Religion = personInformation.Religion;
+
+
+                    existingPersonInformation.Account.Password = Password;
+
+                    dbContext.Update(existingPersonInformation);
+                    dbContext.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!dbContext.PersonInformations.Any(p => p.Id == personInformation.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index", "Admin");
+            }
+            return View(personInformation);
+        }
+    }
+
+
+
+    //--------------------------------------------DETAIlS
+    public IActionResult Details(string id)
+    {
+        using (var dbContext = new FbtContext())
+        {
+            // Retrieve the PersonInformation with the associated Account
+            var personInformation = dbContext.PersonInformations
+                .Include(p => p.Account)
+                .FirstOrDefault(p => p.Id == id);
+
+            if (personInformation == null)
+            {
+                return NotFound();
+            }
+
+            return View(personInformation);
+        }
+    }
+
+    //--------------------------------------Delete
+    public IActionResult Delete(string id)
+    {
+        using (var dbContext = new FbtContext())
+        {
+            var personInformation = dbContext.PersonInformations.Find(id);
+            if (personInformation == null)
+            {
+                return NotFound();
+            }
+
+            return View(personInformation);
+        }
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(string id)
+    {
+        using (var dbContext = new FbtContext())
+        {
+            var personInformation = dbContext.PersonInformations.Find(id);
+            if (personInformation == null)
+            {
+                return NotFound();
+            }
+
+            // Delete the associated Account first
+            var accountId = personInformation.Id;
+            var account = dbContext.Accounts.Find(accountId);
+            if (account != null)
+            {
+                dbContext.Accounts.Remove(account);
+                dbContext.SaveChanges();
+            }
+
+            // Then, delete the PersonInformation
+            dbContext.PersonInformations.Remove(personInformation);
+            dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 
